8870b9fd83446025887c20e14f49dbd797616d74
胡文凡 <wenfan.hu@infaith.com.cn>
2022-10-19 14:09:15 +0800

    feat: add ts_component

---
+++ ts_manually/src/EditUser.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>{{ isEdit? `Edit`: `Create`}} User</h1>
+  <div>
+    <span>Username: </span><input {{user?.name}}/>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
