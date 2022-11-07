d6f46082bf55e3736edd7b8c093220e5bb130279
胡文凡 <wenfan.hu@infaith.com.cn>
2022-11-07 16:29:15 +0800

    up

---
+++ ts_component/src/Child.vue
@@ -0,0 +1,18 @@
+<template>
+  {{value}}
+</template>
+
+<script setup lang="ts">
+import {ref, watch} from "vue";
+
+const props = defineProps<{count: number}>()
+const value = ref<number>(props.count)
+
+watch(props, () => {
+  value.value = props.count
+})
+</script>
+
+<style scoped>
+
+</style>
